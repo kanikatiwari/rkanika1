8b987728d79dccd0ecfa50bace73a7cfd5daf676
kanika <kanika.tiwari@gmail.com>
2017-08-08 18:12:09 +0530

    start

---
+++ starting R.R
@@ -0,0 +1,12 @@
+#help-start
+help()
+#assigning values to a variable
+x=23
+#getting result
+x
+#command to generate random nos. for normal distribution
+rnorm(n, mean=0,sd=1)
+rnorm(n=5, mean=0, sd=1)
+rnorm(10)
+#how to load a file for workspace
+load(file.choose("mymba.csv"))
